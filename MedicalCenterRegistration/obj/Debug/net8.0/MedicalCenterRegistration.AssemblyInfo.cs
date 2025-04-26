58b53725db3d9776e988f66511e59ef55c3ab7a9
Patryk Przybylski <pzx110351@student.poznan.merito.pl>
2025-04-26 19:56:19 +0200

    add feed section

--- MedicalCenterRegistration/obj/Debug/net8.0/MedicalCenterRegistration.AssemblyInfo.cs
+++ MedicalCenterRegistration/obj/Debug/net8.0/MedicalCenterRegistration.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedicalCenterRegistration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdf18dcb33d2f5dafb19a8a2678364de2a9ef633")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03b6c22a7c3fe1612182ea9f287b95392d771dd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedicalCenterRegistration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedicalCenterRegistration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
