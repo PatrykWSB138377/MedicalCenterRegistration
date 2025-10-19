19838a0025d319454c30aba9e8177c92a54616c6
pawel <uciuany@gmail.com>
2025-10-19 20:45:28 +0200

    Dodany panel userów i jakieś drobne poprawki

---
+++ MedicalCenterRegistration/Models/ViewModels/UserListItemViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MedicalCenterRegistration.Models.ViewModels
+{
+    public class UserListItemViewModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string? Role { get; set; }
+        public DateTime? CreatedAt { get; set; }
+    }
+}
