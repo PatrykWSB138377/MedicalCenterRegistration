74ecc0a27821681b471bd9099e66642d10770777
Patryk Przybylski <pzx110351@student.poznan.merito.pl>
2025-10-22 16:38:27 +0200

    refactor navbar actions

---
+++ MedicalCenterRegistration/Enums/NavbarAction.cs
@@ -0,0 +1,10 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace MedicalCenterRegistration.Enums
+{
+    public enum NavbarAction
+    {
+        CreateVisit, MyVisits, PatientCard, AddDoctor, AddSpecialization, AssignSpecialization, UserManagement
+    }
+}
