feef8982097771c497d6a8acdebf5b5948ee0574
Patryk Przybylski <pzx110351@student.poznan.merito.pl>
2025-10-29 21:49:05 +0100

    controller change

---
+++ MedicalCenterRegistration/Controllers/UserFilesController.cs
@@ -0,0 +1,39 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using MedicalCenterRegistration.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedicalCenterRegistration.Controllers
+{
+    public class UserFilesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserFilesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult DownloadFile(string filePath, string fileName)
+        {
+            if (string.IsNullOrEmpty(filePath))
+                return NotFound();
+
+            var fullFilePath = Path.Combine(Directory.GetCurrentDirectory(), filePath);
+
+            if (!System.IO.File.Exists(fullFilePath))
+                return NotFound();
+
+            var contentType = "application/octet-stream";
+            return PhysicalFile(fullFilePath, contentType, fileName);
+        }
+
+        private bool UserFileExists(int id)
+        {
+            return _context.UserFile.Any(e => e.Id == id);
+        }
+    }
+}
